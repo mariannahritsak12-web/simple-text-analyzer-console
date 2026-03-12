cba6571429076e9fc46e02cf065a9df703ebd5aa
Marianna <mariannahritsak12@gmail.com>
2026-03-12 20:54:15 +0100

    Added stop words list and text preprocessing function that normalizes the text and splits into words. Also added unique word counting

--- text-analyzer.py
+++ text-analyzer.py
@@ -1,24 +1,25 @@
-from fileinput import filename
 import string
 import csv
 import os
-
 
 # Load the text from the specified file
 def load_text(file_location):
     with open(file_location, "r") as text_file:
         return text_file.read()
 
-# Remove the punctuation characters
-def clean_text(text):
+# Split the text into words, remove punctuation and convert to lowercase
+def preprocess_text(text):
     text = text.lower()
-    clean_text = "".join(c for c in text if c not in string.punctuation)
-
-    return clean_text
+    normalized_text = "".join(c for c in text if c not in string.punctuation)
+    split_text = normalized_text.split()
+    return split_text
 
 # Count words in the given text
 def count_words(text):
-    return len(text.split())
+    STOP_WORDS = {'a', 'an', 'the', 'and', 'or', 'but', 'if', 'while', 'with', 'to', 'of'}
+    words = preprocess_text(text)
+    filtered_words = [word for word in words if word not in STOP_WORDS]
+    return len(filtered_words)
 
 # Count letters
 def count_letters(text):
@@ -49,6 +50,28 @@ def avg_word_len(text):
         total_lenghth += len(word)
     
     return total_lenghth / len(words)
+
+# Count the number of sentences in the text
+def count_sentences(text):
+    sentence_endings = ['.', '!', '?']
+    sentences = 0
+    
+    for char in text:
+      if char in sentence_endings:
+          sentences += 1
+
+    return sentences
+
+# Unique words in the text
+def unique_words():
+    words = preprocess_text(text)
+    unique_words = set()
+
+    for word in words:
+        if word not in unique_words:
+            unique_words.add(word)
+        
+    return len(unique_words)
 
 # Generate report containing all information about proccessed text
 def generateReport(words, letters, top_words, avg_len):
